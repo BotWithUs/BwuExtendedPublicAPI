cbb603d5537b7e0d7e565f8c760fcf5881f5ef59
Proxify <5466312+Proxify@users.noreply.github.com>
2025-06-18 03:49:16 -0500

    Revert "Incremented gradle version"
    This reverts commit 43059df8c6b7b8666a6344238eb566a3f294051a.

--- src/main/java/net/botwithus/api/game/script/v2/BwuScriptv2.java
+++ src/main/java/net/botwithus/api/game/script/v2/BwuScriptv2.java
@@ -34,6 +34,8 @@ public abstract class BwuScriptv2 extends PermissiveScript {
         super(scriptName, scriptConfig, scriptDef);
     }
 
+
+
     @Override
     public boolean onInitialize() {
         var init = super.onInitialize();
